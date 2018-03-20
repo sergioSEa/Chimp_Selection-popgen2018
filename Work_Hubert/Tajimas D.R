8121072af32c10afc775303cae85d07b28b52185
gool1995 <lizuqi1995@163.com>
2018-03-21 00:49:34 +0100

    Add files via upload

--- Work_Hubert/Tajimas D.R
+++ Work_Hubert/Tajimas D.R
@@ -2,10 +2,11 @@ chimp_data = read.table('F:\\courses\\Population Genetics\\Project\\ChimpExome\\
 schwein_data = chimp_data[1:11,7:46679]
 trogl_data = chimp_data[12:23,7:46679]
 verus_data = chimp_data[24:29,7:46679]    # outgroup
+rm(chimp_data)
 
 # define ancestral alleles
 ances_allele = rep('', 46673)
-for(i in 1:100){
+for(i in 1:46673){
   count = list('A'=0, 'T'=0, 'C'=0, 'G'=0)
   for(j in 1:6){
     if(verus_data[j,i] == '0 0')
@@ -35,8 +36,10 @@ cal_derived_AF = function(genotype){
     derived = 0
     for(j in 1:site_num[1]){
       total = total + 2
-      if(genotype[j,i] == '0 0')
-        next
+      if(genotype[j,i] == '0 0'){
+        derived = total = 1
+        break
+      }
       allele = unlist(strsplit(as.character(genotype[j,i]), ' '))
       if(allele[1] != ances_allele[i]){
         derived = derived + 1
@@ -79,12 +82,18 @@ cal_tajima_D = function(sfs, n){
 sch_tajima_D = rep(0, 46)
 for(i in 1:46){
   sch_derived_AF = cal_derived_AF(schwein_data[,((i-1)*1000+1):(i*1000)])
-  sch_sfs = prop.table(table(sch_derived_AF))
+  sch_sfs = table(sch_derived_AF)
+  # get rid of fixed sites
+  if(names(sch_sfs[1]) == '0')
+    sch_sfs = sch_sfs[-1]
+  # get rid of SNPs with missing genotype
+  if(names(sch_sfs[length(sch_sfs)]) == '1')
+    sch_sfs = prop.table(sch_sfs[-length(sch_sfs)])
   #sch_sfs = c(0.39, 0.19, 0.13, 0.1, 0.08, 0.06, 0.06)
   #names(sch_sfs) = c('0.125', '0.25', '0.375', '0.5', '0.625', '0.75', '0.875')
   sch_tajima_D[i] = cal_tajima_D(sch_sfs, 22)
 }
-plot(1:46, sch_tajima_D, ylim=c(-2, 2), xlab='genome', ylab="Tajima's D", main='Schwein population')
+plot(1:46, sch_tajima_D, ylim=c(-2, 2), xlab='genome', ylab="Tajima's D", main='Schwein population by window')
 abline(h=0, col='red')
 fit1 = lm(sch_tajima_D ~ 1)
 abline(fit1, col='blue')
@@ -93,59 +102,75 @@ abline(fit1, col='blue')
 tro_tajima_D = rep(0, 46)
 for(i in 1:46){
   tro_derived_AF = cal_derived_AF(trogl_data[,((i-1)*1000+1):(i*1000)])
-  tro_sfs = prop.table(table(tro_derived_AF))
-  #sch_sfs = c(0.39, 0.19, 0.13, 0.1, 0.08, 0.06, 0.06)
-  #names(sch_sfs) = c('0.125', '0.25', '0.375', '0.5', '0.625', '0.75', '0.875')
-  tro_tajima_D[i] = cal_tajima_D(tro_sfs, 22)
+  tro_sfs = table(tro_derived_AF)
+  # get rid of fixed sites
+  if(names(tro_sfs[1]) == '0')
+    tro_sfs = tro_sfs[-1]
+  # get rid of SNPs with missing genotype
+  if(names(tro_sfs[length(tro_sfs)]) == '1')
+    tro_sfs = prop.table(tro_sfs[-length(tro_sfs)])
+  tro_tajima_D[i] = cal_tajima_D(tro_sfs, 24)
 }
-plot(1:46, tro_tajima_D, ylim=c(-2, 2), xlab='genome', ylab="Tajima's D", main='Trogl population')
+plot(1:46, tro_tajima_D, ylim=c(-2, 2), xlab='genome', ylab="Tajima's D", main='Trogl population by window')
 abline(h=0, col='red')
 fit2 = lm(tro_tajima_D ~ 1)
 abline(fit2, col='blue')
 
-# 5211 1
-# 3188 2
-# 2544 3
-# 2038 4
-# 2226 5
-# 2575 6
-# 1973 7
-# 1618 8
-# 2031 9
-# 2134 10
-# 2874 11
-# 2387 12
-# 1015 13
-# 1465 14
-# 1444 15
-# 1859 16
-# 2611 17
-# 894 18
-# 2916 19
-# 1310 20
-# 657 21
-# 982 22
-# 721 23
+
 # chromosome-wise
-index = c(5211,3188,2544,2038,2226,2575,1973,1618,2031,2134,2874,2387,1015,
-          1465,1444,1859,2611,894,2916,1310,657,982,721)
-tro_tajima_D = rep(0, 23)
-loc = 1
-for(i in 1:23){
-  tro_derived_AF = cal_derived_AF(trogl_data[,loc:(loc+index[i]-1)])
-  loc = loc + index[i]
-  tro_sfs = prop.table(table(tro_derived_AF))
-  #sch_sfs = c(0.39, 0.19, 0.13, 0.1, 0.08, 0.06, 0.06)
-  #names(sch_sfs) = c('0.125', '0.25', '0.375', '0.5', '0.625', '0.75', '0.875')
-  tro_tajima_D[i] = cal_tajima_D(tro_sfs, 22)
+plot_tajimasD_among_chr = function(pop_data, n, pop_name){
+  index = c(5211,3188,2544,2038,2226,2575,1973,1618,2031,2134,2874,2387,1015,
+            1465,1444,1859,2611,894,2916,1310,657,982,721)
+  pop_tajima_D = rep(0, 23)
+  loc = 1
+  for(i in 1:23){
+    pop_derived_AF = cal_derived_AF(pop_data[,loc:(loc+index[i]-1)])
+    loc = loc + index[i]
+    pop_sfs = table(pop_derived_AF)
+    # get rid of fixed sites
+    pop_sfs = pop_sfs[-1]
+    # get rid of SNPs with missing genotype
+    pop_sfs = prop.table(pop_sfs[-length(pop_sfs)])
+    pop_tajima_D[i] = cal_tajima_D(pop_sfs, n)
+  }
+  plot(1:23, pop_tajima_D, ylim=c(-2, 2), xlab='genome', ylab="Tajima's D", main=paste(pop_name,'population among chr'))
+  abline(h=0, col='red')
+  fit = lm(pop_tajima_D ~ 1)
+  abline(fit, col='blue')
 }
-plot(1:23, tro_tajima_D, ylim=c(-2, 2), xlab='genome', ylab="Tajima's D", main='Trogl population')
-abline(h=0, col='red')
-fit1 = lm(tro_tajima_D ~ 1)
-abline(fit1, col='blue')
+
+plot_tajimasD_among_chr(schwein_data, 22, 'Schwein')
+plot_tajimasD_among_chr(trogl_data, 24, 'Trogl')
 
 
+# for each chromosome
+plot_tajimasD_within_chr = function(pop_data, n, pop_name, dots){
+  index = c(5211,3188,2544,2038,2226,2575,1973,1618,2031,2134,2874,2387,1015,
+            1465,1444,1859,2611,894,2916,1310,657,982,721)
+  loc = 1
+  for(i in 1:23){
+    chr_data = pop_data[,loc:(loc+index[i]-1)]
+    loc = loc + index[i]
+    chr_step = floor(index[i]/dots)
+    pop_tajima_D = rep(0, dots)
+    for(j in 1:dots){
+      pop_derived_AF = cal_derived_AF(chr_data[,((j-1)*chr_step+1):(j*chr_step)])
+      pop_sfs = table(pop_derived_AF)
+      # get rid of fixed sites
+      if(names(pop_sfs[1]) == '0')
+        pop_sfs = pop_sfs[-1]
+      # get rid of SNPs with missing genotype
+      if(names(pop_sfs[length(pop_sfs)]) == '1')
+        pop_sfs = prop.table(pop_sfs[-length(pop_sfs)])
+      pop_tajima_D[j] = cal_tajima_D(pop_sfs, n)
+    }
+    plot(1:dots, pop_tajima_D, ylim=c(-2, 2), xlab='genome', ylab="Tajima's D", main=paste(pop_name,'population within chr',i))
+    abline(h=0, col='red')
+    fit = lm(pop_tajima_D ~ 1)
+    abline(fit, col='blue')
+  }
+}
 
-
-
+plot_tajimasD_within_chr(schwein_data, 22, 'Schwein', 10)
+plot_tajimasD_within_chr(trogl_data, 24, 'Trogl', 10)
 
